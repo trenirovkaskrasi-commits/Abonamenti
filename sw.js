a243609b68ff1cb45ea82705e418e26c04bdd2d7
trenirovkaskrasi-commits <trenirovkaskrasi@gmail.com>
2026-02-04 18:59:29 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'trainer-v20';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
