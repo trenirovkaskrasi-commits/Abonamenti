b43f6505800f5b1419e90998b365bb75ece00feb
trenirovkaskrasi-commits <trenirovkaskrasi@gmail.com>
2026-02-04 20:38:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,10 @@
-const cacheName = 'trainer-app-v20';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const cacheName = 'trainer-v20';
+const assets = ['./', './index.html'];
 
-// Инсталиране на Service Worker и кеширане на файловете
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Активиране и изтриване на стари кешове
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== cacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Обслужване на заявки (зареждане от кеша, ако сме офлайн)
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
